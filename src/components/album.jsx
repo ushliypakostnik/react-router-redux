248509fe6b37e9610052bc422e9409b55948eb72
Levon <levon.gambaryan@gmail.com>
2019-05-16 02:36:02 +0300

    add gallery

---
+++ src/components/album.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { fetch } from 'whatwg-fetch';
+
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
+
+class Album extends Component {
+  constructor() {
+    super();
+    this.state = { currentImage: 0 };
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Gallery
+          photos={this.props.photos}
+          margin="0"
+          targetRowHeight={400}
+          onClick={this.openLightbox} />
+        <Lightbox
+          images={this.props.photos}
+          onClose={this.closeLightbox}
+          onClickPrev={this.gotoPrevious}
+          onClickNext={this.gotoNext}
+          currentImage={this.state.currentImage}
+          isOpen={this.state.lightboxIsOpen}
+        />
+      </div>
+    )
+  }
+}
+
+export default Album;
--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { fetch } from 'whatwg-fetch';
 
+import Album from './album';
+
 class Page extends Component {
   constructor(props) {
     super(props);
@@ -9,8 +11,7 @@ class Page extends Component {
       isLoaded: false,
       images: []
     };
-    this.contentUrl = "http://127.0.0.1:8082";
-    this.fetchUrl = this.contentUrl + "/albums/album" + this.props.id;
+    this.fetchUrl = "http://127.0.0.1:8082/albums/album" + this.props.id;
   }
 
   componentDidMount() {
@@ -34,16 +35,15 @@ class Page extends Component {
 
   render() {
     const { error, isLoaded, images } = this.state;
+
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
       return <div>Loading...</div>;
     } else {
       return (
-        <section className="app__page" test={this.props.key}>
-          {images.map((image, index) => (
-            <img key={index} src={this.contentUrl + image[0]} />
-          ))}
+        <section className="app__page">
+          <Album photos={images} />
         </section>
       );
     }
