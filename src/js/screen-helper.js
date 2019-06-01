c8f6a0d42f0c8fd464b1a93f30bfd87e06ae43f9
Levon <levon.gambaryan@gmail.com>
2019-06-02 02:45:59 +0300

    fixes bugs on mobile

--- src/js/screen-helper.js
+++ src/js/screen-helper.js
@@ -3,10 +3,10 @@ const ScreenHelper = (() => {
   /* eslint-disable no-unused-vars */
   const NAME = 'BootstrapHelper';
 
-  const SM = 576;
-  const MD = 768;
-  const LG = 992;
-  const XL = 1200;
+  const XS = 480;
+  const SM = 768;
+  const MD = 992;
+  const LG = 1200;
 
   function isXS() {
     return window.matchMedia(`(max-width: ${SM - 1}px)`).matches;
@@ -24,11 +24,7 @@ const ScreenHelper = (() => {
 
   function isLG() {
     return window.matchMedia(
-      `(min-width: ${LG}px) and (max-width: ${XL - 1}px)`).matches;
-  }
-
-  function isXL() {
-    return window.matchMedia(`(min-width: ${XL}px)`).matches;
+      `(min-width: ${LG}px)`).matches;
   }
 
   function getScrollbarWidth() {
@@ -45,7 +41,6 @@ const ScreenHelper = (() => {
     isSM,
     isMD,
     isLG,
-    isXL,
     getScrollbarWidth
   };
 })();
